4562f0f058399dc7810a92a91580b027417352ba
Abby Christens <achristens@gmail.com>
2017-10-18 18:05:31 -0400

    Adding static landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'products#index'
 
   resources :products do
     resources :reviews, except: [:index, :new, :show]
   end
+
+  get "pages/:page" => "pages#show"
+  root "pages#show", page: "landing_page"
 
 end
