c5d7ffec7cf76078d43d0b3c6f74ddbdb59e93b5
Abby Christens <achristens@gmail.com>
2017-10-18 13:42:22 -0400

    Adding flash hashes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,6 +11,7 @@ class ProductsController < ApplicationController
     @product.price_in_cents = params[:product][:price_in_cents]
 
     if @product.save
+      flash[:notice] = "Product has been successfully created."
       redirect_to product_path(@product)
     else
       render :new
@@ -30,11 +31,23 @@ class ProductsController < ApplicationController
   end
 
   def update
+    @product = Product.find(params[:id])
+    @product.name = params[:product][:name]
+    @product.description = params[:product][:description]
+    @product.price_in_cents = params[:product][:price_in_cents]
+
+    if @product.save
+      flash[:notice] = "Product has been successfully updated."
+      redirect_to product_path(@product)
+    else
+      render :edit
+    end
   end
 
   def destroy
     @product = Product.find(params[:id])
     if @product.destroy
+      flash[:notice] = "Product has been successfully deleted."
       redirect_to products_path
     else
       render :show
