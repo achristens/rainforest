04cf36d2554c1ee915d2675cfc99a8a12f912dee
Abby Christens <achristens@gmail.com>
2017-10-18 11:45:54 -0400

    Add show view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,14 +11,23 @@ class ProductsController < ApplicationController
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+    @product = Product.find(params[:id])
+    if @product.destroy
+      redirect_to products_path
+    else
+      render :show
+    end
   end
+  
 end
