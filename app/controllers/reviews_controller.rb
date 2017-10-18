0c401eb6c71480e7749414e2f1e86fb7bd7a23a3
Abby Christens <achristens@gmail.com>
2017-10-18 16:19:47 -0400

    Add review form partial

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,17 @@
 class ReviewsController < ApplicationController
 
   def create
+    @review = Review.create
+    @product = Product.find(params[:product_id])
+    @review.review = params[:review][:review]
+    @review.product = @product
+
+    if @review.save
+      flash.notice = "Review has been successfully created."
+      redirect_to @product
+    else
+      render "products/show"
+    end
   end
 
   def edit
@@ -11,5 +22,5 @@ class ReviewsController < ApplicationController
 
   def destroy
   end
-  
+
 end
